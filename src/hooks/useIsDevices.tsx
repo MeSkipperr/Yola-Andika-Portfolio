c1fe2cf705a281e81c181bc664f2c958d06f1db5
MeSkipperr <ikadekyolaandika02@gmail.com>
2025-02-17 09:49:17 +0800

    not important

--- src/hooks/useIsDevices.tsx
+++ src/hooks/useIsDevices.tsx
@@ -7,7 +7,7 @@ type DeviceType = "mobile" | "tablet" | "desktop";
 const getDeviceType = (width: number): DeviceType => {
     if (width < 768) return "mobile";
     if (width < 1024) return "tablet";
-      return "desktop";
+    return "desktop";
 };
 
 export const useIsDevice = () => {
