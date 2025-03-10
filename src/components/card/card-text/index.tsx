b380c18bf962ee56ff4dccde03364f18118bd5a4
MeSkipperr <ikadekyolaandika02@gmail.com>
2025-03-10 15:47:42 +0800

    photoboth project start

--- src/components/card/card-text/index.tsx
+++ src/components/card/card-text/index.tsx
@@ -14,7 +14,7 @@ type TextCardProps = {
 
 const TextCardList = ({ data }: TextCardProps) => {
     return (
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
             {data.map((item, index) => (
                 <div 
                     key={index} 
